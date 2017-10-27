79128897cc08e5326ef0acd817271069609e90c3
Mitch Long <meechllada@gmail.com>
2017-10-27 10:29:51 -0700

    add structure to root component for each animal entry

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <div class="container">
+    <h1>List of Animals</h1>
+    <div>
+    <h3>Name:</h3>
+      <div class="row">
+        <div class="col-sm-6">
+          <h4>Species:</h4>
+          <h4>Name:</h4>
+          <h4>Location:</h4>
+        </div>
+        <div class="col-sm-6">
+          <h4>Caretakers:</h4>
+          <h4>Likes:</h4>
+          <h4>Dislikes:</h4>
+        </div>
+      </div>
+      <hr>
+    </div>
+  </div>
+
+
   `
 })
 
